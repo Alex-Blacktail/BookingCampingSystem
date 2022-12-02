c3fd8b310b8a9cce807f73718b0a559253897df7
Alexey Popov <82538809+Alex-Blacktail@users.noreply.github.com>
2022-12-03 01:33:44 +0500

    Добавил проект Domain для хранения доменной логики. Добавил проект Application для хранение бизнесовой логики. Перенес AppUser (IdentityUser) в Domain

---
+++ Backend/src/Booking.System.Domain/Identity/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Booking.System.Domain.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? ThirdName { get; set; }
+    }
+}
--- Backend/src/Booking.System.WebApi/Identity/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Booking.System.WebApi.Identity.Models
-{
-    public class AppUser : IdentityUser
-    {
-        public string DisplayName { get; set; }
-    }
-}
