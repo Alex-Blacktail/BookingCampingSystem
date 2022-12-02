1bc151c166c89ef1974316a21878863a92359f48
Alexey Popov <82538809+Alex-Blacktail@users.noreply.github.com>
2022-12-03 01:37:27 +0500

    Предварительно добавил репозиторий для работы с пользователями. Добавил метод для регистрации нового пользователя

---
+++ Backend/src/Booking.System.Application/Identity/IUserAuthenticationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Booking.System.Application.Identity.DTO;
+
+namespace Booking.System.Application.Identity
+{
+    public interface IUserAuthenticationRepository
+    {
+        Task<IdentityResult> RegisterUserAsync(UserRegistrationDto userRetistrationDto);
+    }
+}
---
+++ Backend/src/Booking.System.Application/Identity/UserAuthenticationRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+
+using Booking.System.Domain.Identity;
+using Booking.System.Application.Identity.DTO;
+
+using AutoMapper;
+
+namespace Booking.System.Application.Identity
+{
+    public class UserAuthenticationRepository : IUserAuthenticationRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        private AppUser? _user;
+
+        public UserAuthenticationRepository(UserManager<AppUser> userManager, IConfiguration configuration, IMapper mapper)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public async Task<IdentityResult> RegisterUserAsync(UserRegistrationDto userRetistrationDto)
+        {
+            var user = _mapper.Map<AppUser>(userRetistrationDto);
+            var result = await _userManager.CreateAsync(user, userRetistrationDto.Password);
+            return result;
+        }
+    }
+}
