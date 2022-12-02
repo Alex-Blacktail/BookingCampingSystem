86046614abfaab4d78d49fbf34a9ed73ec864961
Alexey Popov <82538809+Alex-Blacktail@users.noreply.github.com>
2022-12-03 01:39:39 +0500

    Добавил класс для хранения методов расширения. Предварительно добавил методы для конфигурации идентити и маппингов

---
+++ Backend/src/Booking.System.WebApi/ServiceCollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using Booking.System.WebApi.Data;
+using Booking.System.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Booking.System.Application.Mappings;
+
+namespace Booking.System.WebApi
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services
+                .AddIdentity<AppUser, IdentityRole>(io =>
+                {
+                    io.Password.RequireDigit = true;
+                    io.Password.RequireLowercase = true;
+                    io.Password.RequireUppercase = true;
+                    io.Password.RequireNonAlphanumeric = false;
+                    io.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<SecurityDbContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureMapping(this IServiceCollection services) 
+        {
+            services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+            var mappingConfig = new MapperConfiguration(map =>
+            {
+                map.AddProfile<UserMappingProfile>();
+            });
+
+            services.AddSingleton(mappingConfig.CreateMapper());
+        }
+    }
+}
