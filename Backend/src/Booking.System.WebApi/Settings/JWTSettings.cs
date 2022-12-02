30a2a02934c589e4f6016e641253e027a9747b33
Alexey Popov <82538809+Alex-Blacktail@users.noreply.github.com>
2022-12-03 02:26:57 +0500

    Добавил конфигурацию для JWT и соотвутсвующий класс для вытягивания настроек

---
+++ Backend/src/Booking.System.WebApi/Settings/JWTSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.System.WebApi.Settings
+{
+    public class JWTSettings
+    {
+        public string ValidIssuer { get; set; }
+        public string ValidAudience { get; set; }
+        public string Secret { get; set; }
+        public string ExpiresIn { get; set; }
+    }
+}
